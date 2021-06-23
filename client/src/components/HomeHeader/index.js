43f6da1162df473833a25c4a9e8b5cde98217477
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-23 14:44:14 -0500

    made background image full height

--- client/src/components/HomeHeader/index.js
+++ client/src/components/HomeHeader/index.js
@@ -6,7 +6,7 @@ import { Row, Col } from "react-materialize";
 function HomeHeader() {
     return (
         <Col 
-        style={{width: "100%", backgroundImage: `url(${Background})`}} 
+        style={{width: "100%", minHeight: "942px", backgroundImage: `url(${Background})`}} 
         xl={12}
         s={12}>
             <Row>
