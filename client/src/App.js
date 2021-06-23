83ab1c2889eca656876c829801603d567d949895
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-23 10:21:41 -0500

    added Navbar to App.js so it will be for all pages, added Switch from react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
@@ -6,33 +5,22 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Products from './pages/Products';
 import Contact from './pages/Contact';
+import Navbar from './components/Navbar'
 import M from 'materialize-css'
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    // <Router>
-    //   <Route exact path="/" component={Home} />
-    //   <Route exact path="/login" component={Login} />
-    //   <Route exact path="/register" component={Register} />
-    //   <Route exact path="/products" component={Products} />
-    //   <Route exact path="/contact" component={Contact} />
-    // </Router>
+    <Router>
+      <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/products" component={Products} />
+      <Route exact path="/contact" component={Contact} />
+      </div>
+    </Router>
   );
 }
 
