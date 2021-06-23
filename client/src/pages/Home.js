6956eeb4a3263209737dc6636f85619446a6395e
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-23 11:51:35 -0500

    added HomeHeader to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Jumbotron from "../components/Jumbotron";
+import HomeHeader from "../components/HomeHeader"
 import M from 'materialize-css';
 
 const Home = () => {
     return(
         <div>
+        <HomeHeader />
         <Jumbotron />
         <Jumbotron />
         <Jumbotron />
