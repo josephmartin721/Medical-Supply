42fbcd5fe7c9020f12c1d973e16846461194640a
josephmartin721 <josephmartin721@yahoo.com>
2021-06-23 13:18:28 -0500

    pull to accept changes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-const path = require("path");
-const router = require("express").Router();
-const apiRoutes = require("./api");
-
-router.use("/api", apiRoutes);
-
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/public/index.html"));
-});
-
-module.exports = router;
-=======
 const path = require("path");
 const router = require("express").Router();
 // const apiRoutes = require("./api");
@@ -24,4 +11,3 @@ router.use(function(req, res) {
 });
 
 module.exports = router;
->>>>>>> f2a876757a917e62148556cc7cb2225fd7b018ca
