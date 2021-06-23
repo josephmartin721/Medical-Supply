65e161ddefb1b6d99292ede8ecaa9a6639475908
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-23 12:12:33 -0500

    installed react-materialze

--- client/src/components/HomeHeader/index.js
+++ client/src/components/HomeHeader/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import logo from "../../logo.png";
 import Background from "../../06.png";
+import { Col } from "react-materialize";
 
 function HomeHeader() {
     return (
-        <div style={{backgroundImage: `url(${Background})`}} className="container align-items-center row z-depth-5">
+        <Col 
+        style={{backgroundImage: `url(${Background})`}} 
+        className="container align-items-center row z-depth-5"
+        s={12}>
             <div className="col-2"></div>
             <img className="col-4 img-fluid rounded-circle" alt="profile pic" src={logo}/>
             <div className="col-2"></div> 
             <div className="col-3 card-content">
-                <h1 className="text-white">Procession in the Evolution of ENT Technology</h1>
+                <h1 textClassName="text-white">Procession in the Evolution of ENT Technology</h1>
             </div>
             <div className="col-1"></div>
-        </div>
+        </Col>
     );
 }
 
