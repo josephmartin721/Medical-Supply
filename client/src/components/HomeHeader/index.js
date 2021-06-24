e3fa19eb2b11c08bf297330eac3c1e6327e7e638
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-23 19:24:00 -0500

    removed unnecessary code

--- client/src/components/HomeHeader/index.js
+++ client/src/components/HomeHeader/index.js
@@ -1,6 +1,4 @@
 import React from "react";
-import logo from "../../logo.png";
-import tech from "../../06.png";
 import Background from "../../background.png";
 import { Row, Col } from "react-materialize";
 
@@ -10,18 +8,6 @@ function HomeHeader() {
         style={{width: "100%", minHeight: "942px", backgroundImage: `url(${Background})`}} 
         xl={12}
         s={12}>
-            <Row>
-            <Col s={8} style={{width: "65%"}}>
-               
-            </Col> 
-            <Col s={4} style={{width: "35%"}} className="white-text">
-                <h4 style={{marginTop: "75px"}}>Procession in the Evolution of ENT Technology</h4>
-            </Col>
-            </Row>
-            <Row>
-                <img style={{marginLeft: "20px"}} src={logo} alt="logo" />
-                <img style={{marginLeft: "20px"}} src={tech} alt="tech" />
-            </Row>
         </Col>
     );
 }
