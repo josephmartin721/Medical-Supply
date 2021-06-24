58628b7b20ad4dd403db97f4b948efe8f18217bc
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-23 20:26:45 -0500

    created homedata.json file that will store info that the jumbotron component will use

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Jumbotron from "../components/Jumbotron";
 import HomeHeader from "../components/HomeHeader";
+import homedata from "./homedata.json";
 
 const Home = () => {
     return(
