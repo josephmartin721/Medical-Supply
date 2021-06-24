8c0e420fa1bc23ab369736c5ad913199395c77b5
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-23 20:57:14 -0500

    added columns around card

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Card } from "react-materialize";
+import { Col, Card, Icon } from "react-materialize";
 import HomeHeader from "../components/HomeHeader";
 import logo from "../logo.png";
 // import Jumbotron from "../components/Jumbotron";
@@ -8,8 +8,9 @@ import logo from "../logo.png";
 const Home = () => {
     return(
     <div>
-        <HomeHeader />
-        <div>
+    <HomeHeader />
+    <div>
+        <Col>
             <Card 
             className="blue-grey darken-1"
             closeIcon={<Icon>close</Icon>}
@@ -18,21 +19,24 @@ const Home = () => {
             title="Card title">
                 WITTY CATCH PHRASE HERE!!!!!!!!!!!!!!!!!!!!!!!!!
             </Card>
-            <Col>
-                <img src={logo} alt="logo"/>
-            </Col>
-            <Card
-            actions={[
-                <a key="1" href="#">This is a link</a>
-                ]}
-            className="blue-grey darken-1"
-            closeIcon={<Icon>close</Icon>}
-            revealIcon={<Icon>more_vert</Icon>}
-            textClassName="white-text"
-            title="Card title">
-                Browse Our Products Here
-            </Card>
-        </div>
+        </Col>
+        <Col>
+            <img src={logo} alt="logo"/>
+        </Col>
+        <Col>
+        <Card
+        actions={[
+            <a key="1" href="#">This is a link</a>
+            ]}
+        className="blue-grey darken-1"
+        closeIcon={<Icon>close</Icon>}
+        revealIcon={<Icon>more_vert</Icon>}
+        textClassName="white-text"
+        title="Card title">
+            Browse Our Products Here
+        </Card>
+        </Col>
+    </div>
     </div>
     );
 };
