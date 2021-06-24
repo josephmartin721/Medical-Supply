0a223de2dc97ecc2455332841410cf392a6135e9
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-23 20:45:35 -0500

    import react-materialize on home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,18 @@
 import React from "react";
-import Jumbotron from "../components/Jumbotron";
+import { Col } from "react-materialize";
 import HomeHeader from "../components/HomeHeader";
-import homedata from "./homedata.json";
+// import Jumbotron from "../components/Jumbotron";
+// import homedata from "./homedata.json";
 
 const Home = () => {
     return(
     <div>
         <HomeHeader />
-        {homedata.map(data => (
-            <Jumbotron />
-        ))}
+        <div>
+            <Col>
+                <img src={} alt="logo"/>
+            </Col>
+        </div>
     </div>
     );
 };
