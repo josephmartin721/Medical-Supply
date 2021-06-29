f064c9510f525116bec72fd42d50d2128cad86c9
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-29 00:13:58 -0500

    set top of screen layout

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,7 +1,15 @@
-import React from "react"
+import React from "react";
+import { Row, Col, Card, CardTitle } from "react-materialize";
 
 const Contact = () => {
-
-}
+    return(
+        <Row>
+            <Col s={8}>
+            </Col>
+            <Col s={4}>
+            </Col>
+        </Row>
+    );
+};
 
 export default Contact
