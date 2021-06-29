757364d865fca3857c30c51a73bf3f96de30a3d2
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-29 00:29:09 -0500

    created card with Company Info

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Card, CardTitle } from "react-materialize";
+import { Row, Col, Card, Icon } from "react-materialize";
 import tech from "../images/06.png";
 
 const Contact = () => {
@@ -13,6 +13,18 @@ const Contact = () => {
                 />
             </Col>
             <Col s={4}>
+                <Card
+                style={{marginTop: "50px", marginBottom: "50px", marginRight:"10px", borderRadius:50}} 
+                className="blue-grey darken-1"
+                closeIcon={<Icon>close</Icon>}
+                revealIcon={<Icon>more_vert</Icon>}
+                textClassName="white-text"
+                title="Our Company's Full Name Here">
+                    12345 Address St.
+                    San Antonio, TX 78201
+
+                    Phone: 210-123-4567
+                </Card>
             </Col>
         </Row>
     );
