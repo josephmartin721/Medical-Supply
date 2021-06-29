6052eddc0d4babe4ad8e3bfd91d1975171ff90ef
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-29 11:18:20 -0500

    added form from register page as a template

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,8 +1,17 @@
-import React from "react";
-import { Row, Col, Card, Icon, Parallax } from "react-materialize";
+import React, { useState } from "react"
+import { Row, Col, Card, CardTitle, Icon, Parallax } from "react-materialize";
+import { Input, TextArea, FormBtn } from "../components/Form";
 import tech from "../images/06.png";
 
 const Contact = () => {
+    const [formObject, setFormObject] = useState({})
+
+    // Handles updating component state when the user types into the input field
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        setFormObject({...formObject, [name]: value})
+    };
+
     return(
         <div>
         <Parallax
@@ -44,6 +53,89 @@ const Contact = () => {
             responsiveThreshold: 0
             }}
         />
+        <Row className="center">
+        <Col s={12} className="center">
+        <Card
+        className="blue-grey darken-1"
+        style={{marginTop:"25px", paddingTop:"20px", paddingBottom:"25px", borderRadius:20}}
+        >
+        <h4>Schedule an Appointment Today!</h4>
+        <h5>Please fill out the form below:</h5>
+        <form 
+        textClassName="white-text"
+        style={{paddingBottom:25}}
+        >
+        <Input
+        onChange={handleInputChange}
+        name="username"
+        placeholder="Username"
+        />
+        <Input
+        onChange={handleInputChange}
+        name="password"
+        placeholder="Password"
+        />
+        <Input
+        onChange={handleInputChange}
+        name="first"
+        placeholder="First Name"
+        />
+        <Input
+        onChange={handleInputChange}
+        name="last"
+        placeholder="Last Name"
+        />
+        <Input
+        onChange={handleInputChange}
+        name="dea"
+        placeholder="DEA Number"
+        />
+        <Input
+        onChange={handleInputChange}
+        name="practice"
+        placeholder="Name of Practice"
+        />
+        <Input
+        onChange={handleInputChange}
+        name="address"
+        placeholder="Address"
+        />
+        <Input
+        onChange={handleInputChange}
+        name="city"
+        placeholder="City"
+        />
+        <Input
+        onChange={handleInputChange}
+        name="state"
+        placeholder="State"
+        />
+        <Input
+        onChange={handleInputChange}
+        name="zipcode"
+        placeholder="Zip Code"
+        />
+        <Input
+        onChange={handleInputChange}
+        name="phone"
+        placeholder="Phone"
+        />
+        <Input
+        onChange={handleInputChange}
+        name="email"
+        placeholder="Email"
+        />
+        <FormBtn
+            style={{marginTop:"25px"}}
+        disabled={!(formObject.username && formObject.password && formObject.first && formObject.last && formObject.dea && formObject.address && formObject.city && formObject.state && formObject.zip && formObject.phone && formObject.email)}
+        //   onClick={handleFormSubmit}
+        >
+        Submit
+        </FormBtn>
+        </form>
+        </Card>
+        </Col>
+        </Row>
         </div>
     );
 };
