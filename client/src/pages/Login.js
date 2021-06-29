21a76d6ac742d99a70c695cc124aa32ae8fef5c2
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-28 19:26:04 -0500

    filled out bulk of form component for login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,37 @@
 import React from "react"
+import { Row, Col, Card, Icon } from "react-materialize"
 import M from 'materialize-css'
 
-function Login() {
-
-}
+const Login = () => {
+    return (
+        <form 
+        style={{marginTop: "100px"}} 
+        className="blue-grey darken-1"
+        textClassName="white-text"
+        >
+        <Input
+          onChange={handleInputChange}
+          name="title"
+          placeholder="Title (required)"
+        />
+        <Input
+          onChange={handleInputChange}
+          name="author"
+          placeholder="Author (required)"
+        />
+        <TextArea
+          onChange={handleInputChange}
+          name="synopsis"
+          placeholder="Synopsis (Optional)"
+        />
+        <FormBtn
+          disabled={!(formObject.author && formObject.title)}
+          onClick={handleFormSubmit}
+        >
+          Submit Book
+        </FormBtn>
+      </form>
+    );
+};
 
 export default Login
