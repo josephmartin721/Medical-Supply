8a6a1a72c7675c1c13bfac52322c2f63800352f8
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-29 00:21:52 -0500

    added placeholder image

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Row, Col, Card, CardTitle } from "react-materialize";
+import tech from "../images/06.png";
 
 const Contact = () => {
     return(
         <Row>
             <Col s={8}>
+            <img 
+                style={{marginTop: "50px", marginBottom: "50px", maxWidth:"100%", height:"auto", borderRadius:50}} 
+                src={tech} 
+                alt="tech"
+                />
             </Col>
             <Col s={4}>
             </Col>
