3e9818389fc5ab5f0900ef6ede4e4d9b58036c4d
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-28 23:46:12 -0500

    code cleanup

--- client/src/components/HomeHeader/index.js
+++ client/src/components/HomeHeader/index.js
@@ -5,7 +5,11 @@ import homeheaderpic from "../../images/homeheader.png"
 function HomeHeader() {
     return (
         <Col xl={12} s={12}>
-            <img style={{width: "100%", height: "auto"}} src={homeheaderpic} alt=""/>
+            <img 
+            style={{width: "100%", height: "auto"}} 
+            src={homeheaderpic} 
+            alt=""
+            />
         </Col>
     );
 }
