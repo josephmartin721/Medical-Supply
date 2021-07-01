a77379d6f862bb8b498f69950a57eb574716a8c1
Angela <angelacr@ymail.com>
2021-06-30 21:17:12 -0500

    adding new components

---
+++ client/src/components/ProductLayout/SingleProduct.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card } from "react-materialize";
+
+const SingleProduct = () => {
+    return (
+        <Card>
+            
+        </Card>
+    )
+}
+
+export default SingleProduct;
