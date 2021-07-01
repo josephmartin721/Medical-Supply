f46c2f6680c7c49e841600790c9f1c9bb797d576
josephmartin721 <josephmartin721@yahoo.com>
2021-06-30 19:48:29 -0500

    edited text leftover from boilerplate code

--- controllers/medController.js
+++ controllers/medController.js
@@ -3,32 +3,32 @@ const db = require("../models");
 // Defining methods for the medController
 module.exports = {
   findAll: function(req, res) {
-    db.Book
+    db.Med
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Book
+    db.Med
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Book
+    db.Med
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Book
+    db.Med
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Book
+    db.Med
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
