608c319f15ad7b95f973bfbce723ce0a90d52f5c
josephmartin721 <josephmartin721@yahoo.com>
2021-06-30 19:28:40 -0500

    added modifiable function code to getuser and getproduct

--- routes/api/getproduct.js
+++ routes/api/getproduct.js
@@ -1,2 +1,15 @@
 const router = require("express").Router();
 const Product = require("../../models/Product");
+
+// Product routes
+router.route("/")
+  .get(Product.findAll)
+  .post(Product.create);
+
+router
+  .route("/:id")
+  .get(Product.findById)
+  .put(Product.update)
+  .delete(Product.remove);
+
+module.exports = router;
--- routes/api/getuser.js
+++ routes/api/getuser.js
@@ -1,2 +1,15 @@
 const router = require("express").Router();
 const User = require("../../models/User");
+
+// User routes
+router.route("/")
+  .get(User.findAll)
+  .post(User.create);
+
+router
+  .route("/:id")
+  .get(User.findById)
+  .put(User.update)
+  .delete(User.remove);
+
+module.exports = router;
