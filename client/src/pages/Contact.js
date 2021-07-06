46c23c97f1bc5b171acb9b6d91c123c38d5af4ec
Leticia Mendiola <lmendiola509@gmail.com>
2021-07-05 19:50:25 -0500

    import footer navbar

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Navbar, Footer } from "./components";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import { Card, Row, Col } from "react-materialize";
 
 const Contact = () => {
