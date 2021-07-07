5b2aa9a2b82e7a6871ed0d27c87e15ec748f2f41
Leticia Mendiola <lmendiola509@gmail.com>
2021-07-07 00:06:48 -0500

    added matching page stylings to ProductId.js

--- client/src/pages/ProductId.js
+++ client/src/pages/ProductId.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
-import { Row, Col, Card, CardTitle, Button } from "react-materialize"
+import { Row, Col, Card, CardTitle, Button } from "react-materialize";
+import background from "../images/background.png";
 
 class ProductId extends Component {
     render() {
         return (
-            <div>
+            <Col xl={12} s={12}>
+                <div className="section" style={{backgroundImage: `url(${background})`, height: 150}}>
+                </div>
                 <div>
                     <Card>
                         skjdflkdsjfjlds
                     </Card>
                 </div>
-            </div>
+            </Col>
         )
     }
 }
