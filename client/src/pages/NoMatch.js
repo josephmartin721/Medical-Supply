d9f2b1ce3d2fb04750947cc613a79d565a926230
Leticia Mendiola <lmendiola509@gmail.com>
2021-07-07 16:10:30 -0500

    formatted 404 page

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,16 +1,22 @@
 import React from "react";
+import { Col } from "react-materialize";
 import Jumbotron from "../components/Jumbotron";
+import background from "../images/background.png";
 
 function NoMatch() {
   return (
-    <Jumbotron>
-      <h1>404 Page Not Found</h1>
-      <h1>
-        <span role="img" aria-label="Face With Rolling Eyes Emoji">
-          🙄
-        </span>
-      </h1>
-    </Jumbotron>
+    <Col xl={12} s={12}>
+      <div className="section" style={{backgroundImage: `url(${background})`, height: 150}}>
+      </div>
+      <Jumbotron>
+        <h1 style={{color:"white"}}>404 Page Not Found</h1>
+        <h1>
+          <span role="img" aria-label="Face With Rolling Eyes Emoji">
+            🙄
+          </span>
+        </h1>
+      </Jumbotron>
+    </Col>
   );
 }
 
