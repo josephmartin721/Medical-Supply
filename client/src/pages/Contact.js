580cc91af70daafb7908fd5c332b02067bd0f80a
Leticia Mendiola <lmendiola509@gmail.com>
2021-07-06 21:58:58 -0500

    added navbar background color/ match formatting of other pages

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,87 +1,84 @@
 import React, { Component } from "react";
 import { Card, Row, Col, SearchForm } from "react-materialize";
 import homeheader from "../images/homeheader.png";
+import background from "../images/background.png";
 import "./style.css";
 
-
-
 class Contact extends Component {
-    render() {
-        return (
-            <div>
-                <Row>
-                  <Col>
-                    <img src={homeheader} id="logo"
-                    width="500"
-                    height="250"
-                    />
-                  </Col>
-
-                  
-                  <Col>
-                    <Card s={6} m={6} className="teal center card-panel s12 m12" id="info">
-                        Company Name
-                        <br/>
-                        <br/>
-                        1234 Address St,
-                        Address, ST 11111
-                        <br/>
-                        <br/>
-                        Phone: 123-456-7890
-                        <br/>
-                        <br/>
-                        Email: email@email.com
-                    </Card>
-                  </Col>
-                </Row>
-
-                <Row>
-                  <header className="center" id="appointment">
-                  Schedule an Appointment Today!
-                  </header>
-                </Row>
-
-                <div class="row">
-    <form class="col s12">
-      <div class="row">
-        <div class="input-field col s6">
-          <input id="name" type="text" class="validate"/>
-          <label for="name">Contact Full Name</label>
-        </div>
-        <div class="input-field col s6">
-          <input id="phone" type="text" class="validate"/>
-          <label for="phone">Contact Phone Number</label>
-        </div>
+  render() {
+    return (
+    <Col xl={12} s={12}>
+      <div className="section" style={{backgroundImage: `url(${background})`, height: 100}}>
       </div>
+      <Row>
+        <Col>
+          <img src={homeheader} id="logo"
+          width="500"
+          height="250"
+          />
+        </Col>
+        <Col>
+          <Card s={6} m={6} className="teal center card-panel s12 m12" id="info">
+          Company Name
+          <br/>
+          <br/>
+          1234 Address St,
+          Address, ST 11111
+          <br/>
+          <br/>
+          Phone: 123-456-7890
+          <br/>
+          <br/>
+          Email: email@email.com
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <header className="center" id="appointment">
+        Schedule an Appointment Today!
+        </header>
+      </Row>
       <div class="row">
-        <div class="input-field col s12">
-          <input id="email" type="email" class="validate"/>
-          <label for="email">Contact Email</label>
+        <form class="col s12">
+        <div class="row">
+          <div class="input-field col s6">
+            <input id="name" type="text" class="validate"/>
+            <label for="name">Contact Full Name</label>
+          </div>
+          <div class="input-field col s6">
+            <input id="phone" type="text" class="validate"/>
+            <label for="phone">Contact Phone Number</label>
+          </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="business" type="text" class="validate"/>
-          <label for="business">Name of Practice</label>
+        <div class="row">
+          <div class="input-field col s12">
+            <input id="email" type="email" class="validate"/>
+            <label for="email">Contact Email</label>
+          </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input placeholder="City, State, Zip Code" id="text" type="address" class="validate"/>
-          <label for="text">Address of Practice</label>
+        <div class="row">
+          <div class="input-field col s12">
+            <input id="business" type="text" class="validate"/>
+            <label for="business">Name of Practice</label>
+          </div>
         </div>
-      </div>
-      <div class="row">
+        <div class="row">
+          <div class="input-field col s12">
+            <input placeholder="City, State, Zip Code" id="text" type="address" class="validate"/>
+            <label for="text">Address of Practice</label>
+          </div>
+        </div>
+        <div class="row">
           <div class="input-field col s12">
             <input id="practice" type="text" class="validate"/>
             <label for="practice">Product(s) in question</label>
           </div>
+        </div>
+        </form>
       </div>
-    </form>
-  </div>
-              </div>
-        )
-    }
+    </Col>
+    )
+  }
 }
 
 export default Contact
