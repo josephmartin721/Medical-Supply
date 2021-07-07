25347b0384a38b8d39a36613b6c9e09e84d6af0b
Angela <angelacr@ymail.com>
2021-07-07 18:18:26 -0500

    got rid of double background in products, made component calendly

--- client/src/pages/ProductId.js
+++ client/src/pages/ProductId.js
@@ -5,15 +5,9 @@ import background from "../images/background.png";
 class ProductId extends Component {
     render() {
         return (
-<<<<<<< HEAD
-            <div>
-                <div className="section" style={{backgroundImage: `url(${background})`, height: 100}}>
-              </div>
-=======
             <Col xl={12} s={12}>
                 <div className="section" style={{backgroundImage: `url(${background})`, height: 150}}>
                 </div>
->>>>>>> main
                 <div>
                     <Card>
                         skjdflkdsjfjlds
--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -6,7 +6,6 @@ import Clearview from "../images/Clearview.jpg";
 import HDS from "../images/HDS.png";
 import DisposableBSS from "../images/DisposableBSS.jpg";
 import Rhinofreeze from "../images/Rhinofreeze.png";
-import background from "../images/background.png";
 import "./style.css";
 
 class Products extends Component {
