70e40411f2d60e1ed183f60b81e9e3d538fd0361
josephmartin721 <josephmartin721@yahoo.com>
2021-07-08 15:29:40 -0500

    api/index removed comment bars on code

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
-// const medRoutes = require("./api");
+const medRoutes = require("./api");
 const userRoutes = require("./user");
+const Product = require("../../models/Product");
 
 // Med routes
-// router.use("/api", medRoutes);
-
+router.use("/api", medRoutes);
 router.use("/users", userRoutes);
+router.use("/product", Product);
 
 module.exports = router;
