476dc84a14a37badd7660279b08f01c559b71587
Leticia Mendiola <lmendiola509@gmail.com>
2021-07-07 19:56:10 -0500

    added login and logout functions to utils/API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,11 @@ import axios from "axios";
 export default {
     getMed: function() {
         return axios.get("api/")
+    },
+    login: function() {
+        return axios.post("api/users/login")
+    },
+    logout: function() {
+        return axios.post("api/users/logout")
     }
 }
