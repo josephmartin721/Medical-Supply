50d065b2573fee30200eeb9e0c9b326211ef67ec
Leticia Mendiola <lmendiola509@gmail.com>
2021-07-08 18:27:37 -0500

    added useState & useEffect to handle API call and state set

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Card, CardTitle, Button } from "react-materialize";
 import background from "../images/background.png";
@@ -10,223 +10,221 @@ import DisposableBSS from "../images/2.png";
 import Rhinofreeze from "../images/5.png";
 import "./style.css";
 
-class Products extends Component {
-  state = {
+const Products = () => {
+  const [productState, setProductState] = useState({
     products: {}
-  };
+  });
 
-  componentDidMount() {
-    API.products()
-      .then(res => this.setState({ products: res.data }))
+  useEffect(() => {
+    API.products.then((res) => {
+      setProductState(res)
       .catch(err => console.log(err));
-  }
+  });},[]);
 
-  render() {
-    return (
-      <div>
-        <div className="section" style={{backgroundImage: `url(${background})`, height: 100}}>
-        </div>
-        <div className="container contain" id="contain">
-        {/* New Mapping Function with new ProductCard component */}
-        <Row>
-          {products.map(product => (
-          <ProductCard 
-          id={products.id}
-          name={products.name}
-          slug={products.slug}
-          description={products.description}
-          use={products.use}
-          benefits={products.benefits}
-          />))
-          }
-        </Row>
-        {/* Original "Hard Coded" Product Cards*/}
-        <Row >
-          <Col s={4} className="center">
-            <Card>
-            <div className="card-image">
-            <img src={Clearview}/>
-            </div>
-            <CardTitle>Clearview</CardTitle>
-            <p>kjdsflsjfoijef</p>
-
-            <Link to="/products/:id">
-            <Button
-            node="button"
-            waves="light"
-            className="indigo btn-small"
-            style={{marginBottom: "5px"}}
-            >
-              View
-            </Button>
-            </Link>
-            <br/>
-            <Link to={"/contact"}>
-              <Button 
-              node="button"
-              waves="light"
-              className="indigo btn-small"
-              >
-                Schedule a Meeting
-              </Button>
-            </Link>
-            </Card>
-            </Col>
-            
-              <Col s={4} className="center">
-            <Card>
-            <div className="card-image">
-            <img src={HDS} width="190" height="190"/>
-            </div>
-            <CardTitle>HDS</CardTitle>
-            <p>kjdsflsjfoijef</p>
-
-            <Link to="/products/:id">
-            <Button
-            node="button"
-            waves="light"
-            className="indigo btn-small"
-            style={{marginBottom: "5px"}}
-            >
-              View
-            </Button>
-            </Link>
-            <br/>
-            <Link to={"/contact"}>
-              <Button node="button"
-              waves="light"
-              className="indigo btn-small"
-              >
-                Schedule a Meeting
-              </Button>
-            </Link>
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-        <Col s={4} className="center">
-            <Card>
-            <div className="card-image">
-            <img src={DisposableBSS} width="190" height="190"/>
-            </div>
-            <CardTitle>Disposable BSS</CardTitle>
-            <p>kjdsflsjfoijef</p>
-
-            <Link to="/products/:id">
-            <Button
-            node="button"
-            waves="light"
-            className="indigo btn-small"
-            style={{marginBottom: "5px"}}
-            >
-              View
-            </Button>
-            </Link>
-            <br/>
-            <Link to={"/contact"}>
-              <Button node="button"
-              waves="light"
-              className="indigo btn-small"
-              >
-                Schedule a Meeting
-              </Button>
-            </Link>
-            </Card>
-          </Col>
-          <Col s={4} className="center">
-            <Card>
-            <div className="card-image">
-            <img src={Rhinofreeze} height="190"/>
-            </div>
-            <CardTitle>Rhinofreeze</CardTitle>
-            <p>kjdsflsjfoijef</p>
-            <Link to="/products/:id">
-            <Button
-            node="button"
-            waves="light"
-            className="indigo btn-small"
-            style={{marginBottom: "5px"}}
-            >
-              View
-            </Button>
-            </Link>
-            <br/>
-            <Link to={"/contact"}>
-              <Button node="button"
-              waves="light"
-              className="indigo btn-small"
-              >
-                Schedule a Meeting
-              </Button>
-            </Link>
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-        <Col s={4} className="center">
-            <Card>
-            <div className="card-image">
-            <img src="https://materializecss.com/images/sample-1.jpg"/>
-            </div>
-            <CardTitle>This Product Name</CardTitle>
-            <p>kjdsflsjfoijef</p>
-            <Link to="/products/:id">
-            <Button
-            node="button"
-            waves="light"
-            className="indigo btn-small"
-            style={{marginBottom: "5px"}}
-            >
-              View
-            </Button>
-            </Link>
-            <br/>
-            <Link to={"/contact"}>
-              <Button 
-              node="button"
-              waves="light"
-              className="indigo btn-small"
-              >
-                Schedule a Meeting
-              </Button>
-            </Link>
-            </Card>
-          </Col>
-          <Col s={4} className="center">
-            <Card>
-            <div className="card-image">
-            <img src="https://materializecss.com/images/sample-1.jpg"/>
-            </div>
-            <CardTitle>This Product Name</CardTitle>
-            <p>kjdsflsjfoijef</p>
-
-            <Link to="/products/:id">
-            <Button
-            node="button"
-            waves="light"
-            className="indigo btn-small"
-            style={{marginBottom: "5px"}}
-            >
-              View
-            </Button>
-            </Link>
-            <br/>
-            <Link to={"/contact"}>
-              <Button 
-              node="button"
-              waves="light"
-              className="indigo btn-small"
-              >
-                Schedule a Meeting
-              </Button>
-            </Link>
-            </Card>
-          </Col>
-        </Row>
+  return (
+    <div>
+      <div className="section" style={{backgroundImage: `url(${background})`, height: 100}}>
       </div>
-      </div>
-    )
-  }
+      <div className="container contain" id="contain">
+      {/* New Mapping Function with new ProductCard component */}
+      <Row>
+        {products.map(product => (
+        <ProductCard 
+        id={products.id}
+        name={products.name}
+        slug={products.slug}
+        description={products.description}
+        use={products.use}
+        benefits={products.benefits}
+        />))
+        }
+      </Row>
+      {/* Original "Hard Coded" Product Cards*/}
+      <Row >
+        <Col s={4} className="center">
+          <Card>
+          <div className="card-image">
+          <img src={Clearview}/>
+          </div>
+          <CardTitle>Clearview</CardTitle>
+          <p>kjdsflsjfoijef</p>
+
+          <Link to="/products/:id">
+          <Button
+          node="button"
+          waves="light"
+          className="indigo btn-small"
+          style={{marginBottom: "5px"}}
+          >
+            View
+          </Button>
+          </Link>
+          <br/>
+          <Link to={"/contact"}>
+            <Button 
+            node="button"
+            waves="light"
+            className="indigo btn-small"
+            >
+              Schedule a Meeting
+            </Button>
+          </Link>
+          </Card>
+          </Col>
+          
+            <Col s={4} className="center">
+          <Card>
+          <div className="card-image">
+          <img src={HDS} width="190" height="190"/>
+          </div>
+          <CardTitle>HDS</CardTitle>
+          <p>kjdsflsjfoijef</p>
+
+          <Link to="/products/:id">
+          <Button
+          node="button"
+          waves="light"
+          className="indigo btn-small"
+          style={{marginBottom: "5px"}}
+          >
+            View
+          </Button>
+          </Link>
+          <br/>
+          <Link to={"/contact"}>
+            <Button node="button"
+            waves="light"
+            className="indigo btn-small"
+            >
+              Schedule a Meeting
+            </Button>
+          </Link>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+      <Col s={4} className="center">
+          <Card>
+          <div className="card-image">
+          <img src={DisposableBSS} width="190" height="190"/>
+          </div>
+          <CardTitle>Disposable BSS</CardTitle>
+          <p>kjdsflsjfoijef</p>
+
+          <Link to="/products/:id">
+          <Button
+          node="button"
+          waves="light"
+          className="indigo btn-small"
+          style={{marginBottom: "5px"}}
+          >
+            View
+          </Button>
+          </Link>
+          <br/>
+          <Link to={"/contact"}>
+            <Button node="button"
+            waves="light"
+            className="indigo btn-small"
+            >
+              Schedule a Meeting
+            </Button>
+          </Link>
+          </Card>
+        </Col>
+        <Col s={4} className="center">
+          <Card>
+          <div className="card-image">
+          <img src={Rhinofreeze} height="190"/>
+          </div>
+          <CardTitle>Rhinofreeze</CardTitle>
+          <p>kjdsflsjfoijef</p>
+          <Link to="/products/:id">
+          <Button
+          node="button"
+          waves="light"
+          className="indigo btn-small"
+          style={{marginBottom: "5px"}}
+          >
+            View
+          </Button>
+          </Link>
+          <br/>
+          <Link to={"/contact"}>
+            <Button node="button"
+            waves="light"
+            className="indigo btn-small"
+            >
+              Schedule a Meeting
+            </Button>
+          </Link>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+      <Col s={4} className="center">
+          <Card>
+          <div className="card-image">
+          <img src="https://materializecss.com/images/sample-1.jpg"/>
+          </div>
+          <CardTitle>This Product Name</CardTitle>
+          <p>kjdsflsjfoijef</p>
+          <Link to="/products/:id">
+          <Button
+          node="button"
+          waves="light"
+          className="indigo btn-small"
+          style={{marginBottom: "5px"}}
+          >
+            View
+          </Button>
+          </Link>
+          <br/>
+          <Link to={"/contact"}>
+            <Button 
+            node="button"
+            waves="light"
+            className="indigo btn-small"
+            >
+              Schedule a Meeting
+            </Button>
+          </Link>
+          </Card>
+        </Col>
+        <Col s={4} className="center">
+          <Card>
+          <div className="card-image">
+          <img src="https://materializecss.com/images/sample-1.jpg"/>
+          </div>
+          <CardTitle>This Product Name</CardTitle>
+          <p>kjdsflsjfoijef</p>
+
+          <Link to="/products/:id">
+          <Button
+          node="button"
+          waves="light"
+          className="indigo btn-small"
+          style={{marginBottom: "5px"}}
+          >
+            View
+          </Button>
+          </Link>
+          <br/>
+          <Link to={"/contact"}>
+            <Button 
+            node="button"
+            waves="light"
+            className="indigo btn-small"
+            >
+              Schedule a Meeting
+            </Button>
+          </Link>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+    </div>
+  )
 }
 
 
