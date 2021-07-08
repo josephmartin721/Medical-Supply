9b3639aebdfdc0271f233752f2721a68ee684193
josephmartin721 <josephmartin721@yahoo.com>
2021-07-08 15:38:26 -0500

    comment coding removed and contents of product js added in api index

--- routes/api/index.js
+++ routes/api/index.js
@@ -8,4 +8,16 @@ router.use("/api", medRoutes);
 router.use("/users", userRoutes);
 router.use("/product", Product);
 
+// Product routes
+router.route("/")
+  .get(Product.findAll)
+  .post(Product.create);
+
+// api/products/id
+  router
+  .route("/:id")
+  .get(Product.findById)
+  .put(Product.update)
+  .delete(Product.remove);
+
 module.exports = router;
