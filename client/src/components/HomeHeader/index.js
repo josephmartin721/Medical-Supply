582396edccb20f76faf610ab9131780e4805871a
Leticia Mendiola <lmendiola509@gmail.com>
2021-07-09 21:49:59 -0500

    added Carousel component

--- client/src/components/HomeHeader/index.js
+++ client/src/components/HomeHeader/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Parallax } from "react-materialize";
+import { Parallax, Carousel } from "react-materialize";
 import Jumbotron from "../../components/Jumbotron";
 import homeheaderpic from "../../images/brain.png";
 import pictwo from "../../images/surgery.jpg";
@@ -16,6 +16,27 @@ function HomeHeader() {
         <Jumbotron>
             <h1 style={{color:"white"}}>About Us</h1>
             <h4 style={{color:"white"}}>In 2021, our company started out....</h4>
+            <Carousel
+            carouselId="Carousel-2"
+            images={[
+                'https://picsum.photos/200/300?image=0',
+                'https://picsum.photos/200/300?image=1',
+                'https://picsum.photos/200/300?image=2',
+                'https://picsum.photos/200/300?image=3',
+                'https://picsum.photos/200/300?image=4'
+            ]}
+            options={{
+                dist: -100,
+                duration: 200,
+                fullWidth: false,
+                indicators: false,
+                noWrap: false,
+                numVisible: 5,
+                onCycleTo: null,
+                padding: 0,
+                shift: 0
+            }}
+            />
         </Jumbotron>
         <Parallax
             image={<img alt="" src={pictwo} style={{width: "auto", height: "auto"}}/>}
