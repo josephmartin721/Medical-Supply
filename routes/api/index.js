cd92ca57101108f209e811b8cac4db6bce74d40b
josephmartin721 <josephmartin721@yahoo.com>
2021-07-10 10:45:03 -0500

    index fix medroutes to be possibly renaming

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const medRoutes = require("./api");
 const userRoutes = require("./user");
-const Product = require("../../controllers/productController");
+const productRoutes = require("./product");
 
 // Med routes
 router.use("/api", medRoutes);
 router.use("/users", userRoutes);
-router.use("/product", Product);
+router.use("/product", productRoutes);
 
 module.exports = router;
