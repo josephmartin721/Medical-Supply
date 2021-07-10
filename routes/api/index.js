667dec9e5100859ec284a96fa97ee185a137a2bc
josephmartin721 <josephmartin721@yahoo.com>
2021-07-10 10:26:03 -0500

    added a product controller file and edited the index inside of the api folder

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,23 +1,11 @@
 const router = require("express").Router();
 const medRoutes = require("./api");
 const userRoutes = require("./user");
-const Product = require("../../models/Product");
+const Product = require("../../controllers/productController");
 
 // Med routes
 router.use("/api", medRoutes);
 router.use("/users", userRoutes);
 router.use("/product", Product);
-
-// Product routes
-router.route("/")
-  .get(Product.findAll)
-  .post(Product.create);
-
-// api/products/id
-  router
-  .route("/:id")
-  .get(Product.findById)
-  .put(Product.update)
-  .delete(Product.remove);
 
 module.exports = router;
