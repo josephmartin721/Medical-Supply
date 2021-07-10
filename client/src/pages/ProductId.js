bef8ea3170a9f2938c99dcb28271ec9b62a974af
Angela <angelacr@ymail.com>
2021-07-10 17:48:33 -0500

    updating db

--- client/src/pages/ProductId.js
+++ client/src/pages/ProductId.js
@@ -1,6 +1,6 @@
 import React, { Component, useEffect, useState } from "react";
 import axios from "axios";
-import { Row, Col, Card, CardTitle, Button } from "react-materialize";
+import { Row, Col, Card, CardTitle, Button, Container } from "react-materialize";
 import { Link, useParams } from "react-router-dom";
 import background from "../images/background.png";
 import API from "../utils/API";
@@ -21,7 +21,27 @@ const ProductId = (props) => {
     },[])
     return (
         <div style={{backgroundImage: `url(${background})`, height: 100}}>
-           <h2>{data.name}</h2>
+            <Row>
+                <Col>
+                <Card>
+                    {data.name}
+                </Card>
+                </Col>
+
+                <Col>
+                <Card>
+                    <header>{data.name}</header>
+                    <p>{data.description}</p>
+                </Card>
+
+
+                <Card>
+                        {data.use}
+                        {data.benefits}
+                        </Card>
+                </Col>
+                    
+            </Row>
         </div>
         
     )
