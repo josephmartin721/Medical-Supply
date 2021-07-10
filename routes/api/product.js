ca7a073bf8f1968de0e35b24b9f16acb0c2623fb
josephmartin721 <josephmartin721@yahoo.com>
2021-07-10 10:37:21 -0500

    edit of product call in index

--- routes/api/product.js
+++ routes/api/product.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Product = require("../../models/Product");
+const Product = require("../../controllers/productController");
 
 // Product routes
 router.route("/")
