1e7e535632827016826f2b19a538e2d60c2692ce
Leticia Mendiola <lmendiola509@gmail.com>
2021-07-11 19:22:36 -0500

    added background image to Contact Page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Card, Row, Col, Container } from "react-materialize";
 import background from "../images/background.png";
+import building from "../images/building2.jpg";
 import Calendly from "../components/Calendly/calendly";
 import "./style.css";
 
@@ -8,34 +9,36 @@ import "./style.css";
 class Contact extends Component {
     render() {
         return (
-            <div>
+          <Col xl={12} s={12}>
               <div className="section" style={{backgroundImage: `url(${background})`, height: 150}}>
-                </div>
-                <Row>
-                  <Col s={3}></Col>
-                  <Col s={6} id="info">
-                    <Card  className="indigo center white-text">
-                      <h5>Contact Us</h5>
-                      <hr/>
-                        <p>Company Name</p>
-                        <br/>
-                        <p>1234 Address St,
-                        Address, ST 11111</p>
-                        <br/>
-                        <p>Phone: 123-456-7890</p>
-                        <br/>
-                        <p>Email: email@email.com</p>
-                    </Card>
-                  </Col>
-                  <Col s={3}></Col>
-                </Row>
+              </div>
+              <div style={{backgroundImage: `url(${building})`, backgroundSize: "100%", height: 1750}}>
+              <Row style={{paddingTop: "80px", paddingBottom: "200px"}}>
+                <Col s={3}></Col>
+                <Col s={6} id="info">
+                  <Card  className="indigo center white-text">
+                    <h5>Contact Us</h5>
+                    <hr/>
+                      <p>Company Name</p>
+                      <br/>
+                      <p>1234 Address St,
+                      Address, ST 11111</p>
+                      <br/>
+                      <p>Phone: 123-456-7890</p>
+                      <br/>
+                      <p>Email: email@email.com</p>
+                  </Card>
+                </Col>
+                <Col s={3}></Col>
+              </Row>
                 
               <Container>
                 <Col id="schedule">
                   <Calendly/>
                 </Col>
               </Container>
-            </div>
+              </div>
+            </Col>
         )
     }
 }
